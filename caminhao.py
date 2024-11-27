ee4ddb6c374cf59a69d47905932ce7241fa43689
Ryan <ryan.hideki10@gmail.com>
2024-11-27 01:03:44 -0300

    Implementada funções da classe Caminhao e PostoEncomendas (Auxiliares)
    - IrPraProximoPosto
    - Getters e Setters de PostoEncomenda
    - Variáveis num de PostoEncomenda

--- caminhao.py
+++ caminhao.py
@@ -1,17 +1,39 @@
+from collections import deque
 import time
 import random
+
 
 class Caminhao:
     
     def __init__(self, carga_maxima, dist_proximo_posto):
         self.carga_maxima = carga_maxima
         self.distancia_proximo_posto = dist_proximo_posto
-        self.posto_atual = 0
+        self.posto_atual = None
+        self.fila_encomendas = deque()
+        pass
+    
+    def irProProximoPosto (self, primeiro_posto): # Usar na main durante a primeira viagem do caminhão
+        time.sleep(random.uniform(0.1, 0.5))
+        self.posto_atual = primeiro_posto  
+        
+        for encomenda in self.fila_encomendas:
+            if encomenda.dest == self.posto_atual.getPostoNum():
+                primeiro_posto.receberCaminhao(self)
+            elif primeiro_posto.getFilaDespachoQuantidade() > 0:
+                primeiro_posto.receberCaminhao(self)
         pass
 
+    
     def irProProximoPosto (self):
         time.sleep(random.uniform(0.1, 0.5))
-        self.posto_atual += 1 # Consertar : Quando estiver saindo do último posto, não aumentar 1, mas sim, voltar ao posto 1.
+        self.posto_atual = self.posto_atual.getProximoPosto() # Consertar : Quando estiver saindo do último posto, não aumentar 1, mas sim, voltar ao posto 1.
+        
+        for encomenda in self.fila_encomendas:
+            if encomenda.dest == self.posto_atual.getPostoNum():
+                self.posto_atual.receberCaminhao(self)
+            elif self.posto_atual.getFilaDespachoQuantidade() > 0:
+                self.posto_atual.receberCaminhao(self)
+        
         pass
 
     def pegarPacote (self):
--- posto_encomenda.py
+++ posto_encomenda.py
@@ -1,13 +1,28 @@
 from collections import deque
 
 class postoEncomenda:
+ 
 
-    def __init__(self):
+    def __init__(self, num):
+        self.num = num
+        self.proximo_posto = None # Duranante a criação dos postos, na próxima iteração, setar o próximo posto do posto anterior.
         self.fila_caminhoes = deque()
         self.fila_encomendas = deque()
         self.fila_despacho = deque()
-        
         pass
+
+    def setProxPosto (self, proximo_posto):
+        self.proximo_posto = proximo_posto
+        pass
+    
+    def getProxPosto (self): # Tratar para caso NONE
+        return self.proximo_posto 
+
+    def getPostoNum (self):
+        return self.num
+    
+    def getFilaDesapachoQuantidade(self):
+        return self.fila_despacho.count()
 
     def receberCaminhao (self, caminhao):
         self.fila_caminhoes.append(caminhao)
